7e6de52d2fb555b39d9e6ddf0090c9daf23bf88d
Bashkirtsev <bashkirtsevich@gmail.com>
2015-05-16 23:40:24 +0600

    Basis.

---
+++ dpr/madql.dpr
@@ -0,0 +1,14 @@
+program madql;
+
+uses
+  Vcl.Forms,
+  Forms.Main in '..\src\Forms.Main.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
